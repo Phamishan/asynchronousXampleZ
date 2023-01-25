ae82ebd8275109b7fcdfa60606d27e857cc64a67
ItzPolarBearDK <70688416+ItzPolarBearDK@users.noreply.github.com>
2023-01-25 15:27:24 +0100

    Vi er started på opgaverne

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,64 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Program
+    internal class tankBil { }
+    internal class stoevsugeBil { }
+    internal class koebeHotdog { }
+    internal class snakke { }
+
+    class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args) 
         {
-            Console.WriteLine("hej");
+            tankBil Bil = gasstation();
+            Console.WriteLine("Bil is tanket");
+
+            stoevsugeBil bilInt = await suger();
+            findSF(bilInt);
+
+            Console.WriteLine("Færdig");
+            
+            koebeHotdog hotdog = spisHD();
+            Console.WriteLine("Nom nom nom, hotdog is spiset");
+            
+            
+            snakke samtale = newFriend();
+            Console.WriteLine("Hej med dig jeg hedder Kaj");
         }
+
+        private static tankBil gasstation()
+        {
+            Console.WriteLine("Tanker bil");
+            return new tankBil();
+        }
+
+        private static void findSF(stoevsugeBil bilInt)
+        {
+            Console.WriteLine("Wwwwww");
+            Task.Delay(3000).Wait();
+        }
+        private static stoevsugeBil suger()
+        {
+            Console.WriteLine("Finder støvsuger");
+            Task.Delay(1000).Wait();
+            Console.WriteLine("Start støvsuger");
+            Task.Delay(3000).Wait();
+            return new stoevsugeBil();
+        }
+
+        private static koebeHotdog spisHD()
+        {
+            Console.WriteLine("Køber hotdog");
+            Task.Delay(3000).Wait();
+            return new koebeHotdog();
+        }
+
+        private static snakke newFriend()
+        {
+            Console.WriteLine("Hej med dig Kaj, samtale is snakket");
+            Task.Delay(3000).Wait();
+            return new snakke();
+        }
+
     }
 }
